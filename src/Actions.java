f1a1cc5661526b7fc75e1bb8fac34e6b253ba688
sguyot4 <sylvain.guyot5@gmailcom>
2016-11-19 14:40:49 +0100

    Classe Action, méthodes vides, effet des cartes en commentaire

---
+++ src/Actions.java
@@ -0,0 +1,219 @@
+/**
+ * Created by sguyot4 on 19/11/16.
+ */
+public class Actions extends Cards {
+
+    /**
+     * NOTE : le site que j'ai trouvé pour les actions des cartes était moitié français, moitié allemand
+     * et j'ai pas trouvé d'autres sites. changer quand on aura la boite du jeu
+     * donc tous les (à compléter) sont a
+     * lien : http://www.brettspielwelt.de/Hilfe/Anleitungen/Dominion/Karten/?nation=fr
+     */
+
+    /**
+     *  Piochez dans deck jusqu'a avoir deux cartes trésors
+     *  gardez les trésors dans main
+     *  défaussez autres
+     */
+    public void aventurier() {
+
+    }
+
+    /**
+     * Prendre carte de cout <= 4
+     */
+    public void atelier(){
+
+    }
+
+    /**
+     * Piochez jusqu'a avoir 7 cartes
+     * Mettre action de coté
+     * Une fois 7 cartes, défaussez cartes de coté
+     */
+    public void bibliotheque() {
+
+    }
+
+    /**
+     *  +1 achat possible
+     *  +2 coins
+     */
+    public void bucheron(){
+
+    }
+
+    /** CHIANT
+     * prendre 1 coin
+     * tous les joueurs placent une carte point au dessus deck
+     * si pas de carte point, montre ses cartes
+     */
+    public void bureaucrate() {
+
+    }
+
+    /**
+     * +1 action
+     * (a completer)
+     */
+    public void cave(){
+
+    }
+
+    /**
+     * +2 coins
+     * (à compléter)
+     */
+    public void chancelier(){
+
+    }
+
+    /**
+     * éliminer jusqu'à 4 cartes de la main
+     */
+    public void chapelle(){
+
+    }
+
+    /**
+     * + 2 cartes
+     * (à compléter)
+     */
+    public void douves(){
+
+    }
+
+    /**
+     * +1 carte
+     * +1 action
+     * (à compléter)
+     */
+    public void espion(){
+
+    }
+
+    /**
+     * +2 actions
+     * +1 achat
+     * +2 coin
+     */
+    public void festival(){
+
+    }
+
+    /**
+     * éliminer cette carte
+     * prendre une carte de cout <= 5
+     */
+    public void festin(){
+
+    }
+
+    /**
+     * 1 point de victoire pour chaque série de 10 cartes dans le jeu complet
+     */
+    public void jardins(){
+
+    }
+
+    /**
+     * +2 cartes
+     * +1 action
+     */
+    public void laboratoire(){
+
+    }
+
+    /**
+     * +3 cartes
+     */
+    public void manufacture(){
+
+    }
+
+    /**
+     * +1 cartes
+     * +1 action
+     * +1 achat
+     * +1 coin
+     */
+    public void marche(){
+
+    }
+
+    /**
+     * +2 coin
+     * (à compléter)
+     */
+    public void milice(){
+
+    }
+
+    /**
+     * éliminer une carte trésor de la main
+     * en prendre une d'une valeur <= val+3
+     */
+    public void mine(){
+
+    }
+
+    /**
+     * (à compléter)
+     * +3 coin
+     */
+    public void preteur(){
+
+    }
+
+    /**
+     * +1 carte
+     * +2 actions
+     */
+    public void village(){
+
+    }
+
+    /**
+     * 2 premières cartes du deck de chaque adversaire dévoilées
+     * Si carte(s) trésor dévoilée(s), joueur décide d'éliminer celle qu'il veut
+     * Joueur peut prendre autant de carte éliminée qu'il souhaite
+     * Les pose dans sa défausse
+     * les autres vont dans la défausse de leur joueur
+     */
+    public void voleur(){
+
+    }
+
+    /**
+     * éliminer cette carte
+     * jouer une carte de valeur <= val+2
+     */
+    public void renovation(){
+
+    }
+
+    /**
+     * +4 cartes
+     * +1 achat
+     * (à compléter)
+     */
+    public void reunionConseil(){
+
+    }
+
+    /**
+     * jouez deux fois même carte action de la main
+     */
+    public void salleTrone(){
+
+    }
+
+    /**
+     * + 2 cartes
+     * (à compléter)
+     */
+    public void sorciere(){
+
+    }
+
+}
