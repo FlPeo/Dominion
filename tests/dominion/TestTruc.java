671b086fa214b3b6e5800420a84aa9cf6729622b
florian <partyflorian@gmail.com>
2016-11-21 20:09:43 +0100

    ajout de @Test pour pouvoir lancer les tests

--- tests/dominion/TestTruc.java
+++
@@ -1,7 +0,0 @@
-package dominion;
-
-/**
- * Created by florian on 13/11/2016.
- */
-public class TestTruc {
-}
--- tests/dominion/UnitTestVictoireCards.java
+++ tests/dominion/UnitTestVictoireCards.java
@@ -1,41 +1,48 @@
 package dominion;
 import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by sakalypse on 20/11/2016.
  */
 public class UnitTestVictoireCards {
 
+    @Test
     public void domaineVaut1(){
         VictoireCards carte = new VictoireCards();
         carte.domaine();
         Assert.assertEquals(1, carte.reservePoint);
     }
 
+    @Test
     public void reserveDomainePioche1(){
         VictoireCards carte = new VictoireCards();
         carte.domaine();
         Assert.assertEquals(11, carte.reserveCarteDomaine);
     }
 
+    @Test
     public void ducheVaut1(){
         VictoireCards carte = new VictoireCards();
         carte.duche();
         Assert.assertEquals(3, carte.reservePoint);
     }
 
+    @Test
     public void reserveDuchePioche1(){
         VictoireCards carte = new VictoireCards();
         carte.duche();
         Assert.assertEquals(11, carte.reserveCarteDuche);
     }
 
+    @Test
     public void ProvinceVaut6(){
         VictoireCards carte = new VictoireCards();
         carte.province();
         Assert.assertEquals(6, carte.reservePoint);
     }
 
+    @Test
     public void reserveProvincePioche1(){
         VictoireCards carte = new VictoireCards();
         carte.province();
