1bdc1ad70a634e6a9ed4895d2801b008147de25f
sguyot4 <sylvain.guyot5@gmailcom>
2016-11-24 16:09:35 +0100

    Modification de la classe joueur une partie des actions rajoutées dedans

--- src/dominion/Joueur.java
+++ src/dominion/Joueur.java
@@ -17,5 +17,48 @@ public class Joueur
     private List<Cards> main;
     private List<Cards> deck;
 
+    Joueur(String nomJoueur){
+        this.nomJoueur = nomJoueur;
+        coins = 1;
+        nbTourAction = 1;
+        nbTourAchat = 1;
+        pointsVictoires = 0;
+        nbDeCartesADefausser = 0;
+    }
+
+    public int addCoin(int nbAjout){
+        coins += nbAjout;
+        return coins;
+    }
+
+    public int addTourAction(int nbAjout){
+        nbTourAction += nbAjout;
+        return nbTourAction;
+    }
+
+    public int addTourAchat(int nbAjout){
+        nbTourAchat += nbAjout;
+        return nbTourAchat;
+    }
+
+    public int addPoints(int nbAjout){
+        pointsVictoires += nbAjout;
+        return pointsVictoires;
+    }
+
+    public void piocher(int nbCarte){
+        int taille;
+        Cards card;
+        for (int i = 0; i < nbCarte; i++){
+            taille = deck.size();
+            card = deck.get((int)Math.random()*(taille));
+            main.add(card);
+            deck.remove(card);
+        }
+    }
+
+    public void defausser(){
+
+    }
 
 }
