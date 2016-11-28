b22c89afa7f7b839a34610af2c265634e9496f08
kevin limacher <kevin.limacher@gmail.com>
2016-11-28 14:40:26 +0100

    carte coins fait

--- src/dominion/CoinsCards.java
+++ src/dominion/CoinsCards.java
@@ -5,10 +5,37 @@ package dominion;
  */
 public class CoinsCards extends Cards
 {
-    CoinsCards()
-    {
+    private int pointCoin;
+    private int cout;
 
+    CoinsCards(int valeurIdCarteCoins){
+        switch (valeurIdCarteCoins){
+            case 1: pointCoin = 1; //copper
+                cout = 2;
+                break;
+            case 2: pointCoin = 2; //silver
+                cout = 3;
+                break;
+            case 3 : pointCoin = 3; //gold
+                cout = 6;
+                break;
+        }
     }
 
 
+    public int getPointCoin() {
+        return pointCoin;
+    }
+
+    public void setPointCoin(int pointCoin) {
+        this.pointCoin = pointCoin;
+    }
+
+    public int getCout() {
+        return cout;
+    }
+
+    public void setCout(int cout) {
+        this.cout = cout;
+    }
 }
