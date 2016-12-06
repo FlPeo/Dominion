b006e33438bf2318da4efb73d3653cc73e626cc9
Flo <partyflorian@gmail.com>
2016-12-06 20:37:32 +0100

    Update sur les classes de test

---
+++ tests/dominion/PartieUnitTest.java
@@ -0,0 +1,15 @@
+package dominion;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by Flo on 06/12/2016.
+ */
+public class PartieUnitTest {
+    @Ignore
+    @Test
+    public void test(){
+
+    }
+}
