8dac6ea098ee8b8b1157bf9e362a2f25912a4273
sguyot4 <sylvain.guyot5@gmail.com>
2017-01-09 17:32:55 +0100

    review commentaires ActionsCards.java

--- src/dominion/ActionCards.java
+++ src/dominion/ActionCards.java
@@ -12,17 +12,10 @@ import java.util.List;
  */
 public class ActionCards extends Cards {
 
-    /** NOTE :  j'ai rien compris à ce qu'il fallait faire ni comment le faire
-     *  du coup j'ai fait ça sans grande conviction
-     *  pleins de trucs seront a changer mais je pense que la base est là
-     *  aucuns tests fait
-     **/
 
     private List<Action> listeActions;
     private boolean carteADefausserApresAction;
     private static ActionCards carteBoucheTrou = new ActionCards(5, new ArrayList<Action>(), 3);
-    //Supprimer la carte bouche trou des que possible ! (quand on aura fait toutes les cartes action
-
 
     public ActionCards(int id, List<Action> actions, int cout){   //id = numero de l'img de la carte
         super(id, cout);
@@ -52,8 +45,6 @@ public class ActionCards extends Cards {
         return this.carteADefausserApresAction;
     }
 
-
-
     public static ArrayList<ArrayList<ActionCards>> creer10CartesAction(Partie p, int nbJoueurs){
         ActionCards[] listeToutesCartesActions = creer25CartesAction(p);
         Collections.shuffle(Arrays.asList(listeToutesCartesActions));
@@ -61,15 +52,12 @@ public class ActionCards extends Cards {
         return liste10CartesAction;
     }
 
-
     private static ActionCards[] creer25CartesAction(Partie p) {
         //ActionCards[] listeCartesActions = new ActionCards[25];
         ActionCards[] listeCartesActions = new ActionCards[11];
         for(int i = 0; i< listeCartesActions.length ; i++){
             listeCartesActions[i] = carteBoucheTrou;
         }
-
-
 
         List<Action> listeActions = new ArrayList<Action>();
 
@@ -115,20 +103,16 @@ public class ActionCards extends Cards {
         listeActions.add(new ActionAdversairesAddXCartes(p, 1));
         listeCartesActions[7] = new ActionCards(14, listeActions, 5);
 
-
-
         listeActions = new ArrayList<Action>();
         listeActions.add(new ActionAddXCartes(p, 3));  //carte forgeron
         listeCartesActions[8] = new ActionCards(15, listeActions, 4);
 
 
         //21 : jouer carte action deux fois
-        //A tester !!
         listeActions = new ArrayList<Action>();
         listeActions.add(new ActionChoixCarteActionMain(p, 1));  //salle du trone
         listeActions.add(new ActionJouerDeuxFoisCarteChoisie(p, 1));
         listeCartesActions[9] = new ActionCards(21, listeActions, 4);
-
 
         listeActions = new ArrayList<Action>();
         listeActions.add(new ActionAddXActions(p, 2));  //carte festival
@@ -136,16 +120,10 @@ public class ActionCards extends Cards {
         listeActions.add(new ActionAddXCoins(p, 2));
         listeCartesActions[10] = new ActionCards(23, listeActions, 5);
 
-        // Adonis
-
-        // ------
-
-
         return listeCartesActions;
     }
 
     private static ArrayList<ArrayList<ActionCards>> selectionner10Cartes(ActionCards[] listeToutesCartesActions, int nbJoueurs) {
-        //TO DO
         ArrayList<ArrayList<ActionCards>> listeCartes = new ArrayList<ArrayList<ActionCards>>();
         ArrayList<ActionCards> cards;
         int nbCartesJardins = (nbJoueurs == 2)?8:12;
@@ -159,7 +137,6 @@ public class ActionCards extends Cards {
         }
         return listeCartes;
     }
-
 
     public void rempliActionsTour(Partie modelPartie) {
         for(Action a : listeActions){
